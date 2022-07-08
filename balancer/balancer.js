8b5f08a7eeec26f4640868818dcd8e7122000fbd
Yusuf Urgupluoglu <yusurgup@WKMZTF3AD819.global.publicisgroupe.net>
2022-07-08 23:09:59 +0200

    Add server admin port to listen loadbalancer decision changes  (it fails because of the scope)

--- balancer/balancer.js
+++ balancer/balancer.js
@@ -4,11 +4,14 @@ const axios = require("axios")
 const ServerPicker = require("./ServerPicker.js")
 
 const app = express()
-const PORT = process.env.PORT || 8080
+const admin = express()
+const SERVING_PORT = parseInt(process.argv.slice(3)) || 8000
+const ADMIN_PORT = parseInt(process.argv.slice(2)) || 8090
 const serverPicker = new ServerPicker()
+const select = true
 
 const httpHandler = async (req, res) => {
-  const server = serverPicker.pickIncrementally()
+  const server = select ? serverPicker.pickIncrementally() : serverPicker.pickUniformly()
   const { method, url, headers, body } = req
 
   try {
@@ -28,6 +31,11 @@ const httpHandler = async (req, res) => {
 app.get("/favicon.ico", (_, res) => res.sendFile("/logo.svg"))
 app.use((req, res) => httpHandler(req, res))
 
-app.listen(PORT, () =>
-  console.log(`Load balancer server is up and running on PORT ${PORT}...`)
+app.listen(SERVING_PORT, () =>
+  console.log(`Load balancer server is up and running on PORT ${SERVING_PORT}...`)
+)
+
+admin.use((req, res) => select = !select)
+admin.listen(ADMIN_PORT, () =>
+  console.log(`Administrator is up and running on PORT ${ADMIN_PORT}...`)
 )
